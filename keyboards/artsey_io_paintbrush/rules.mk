e38cd0da1c39b20c5d404abf6add417e6e583d0a
Jim McKeeth <jim@mckeeth.org>
2025-10-05 14:56:37 -0600

    Update rules.mk

--- keyboards/artsey_io_paintbrush/rules.mk
+++ keyboards/artsey_io_paintbrush/rules.mk
@@ -1,4 +1,5 @@
+BOOTLOADER = rp2040
 COMBO_ENABLE = yes    
 CONSOLE_ENABLE = yes
-# MOUSEKEY_ENABLE = yes # already enabled in keyboard.json
-# EXTRAKEY_ENABLE = yes # already enabled in keyboard.json
+MOUSEKEY_ENABLE += yes # already enabled in keyboard.json
+EXTRAKEY_ENABLE += yes # already enabled in keyboard.json
