869cc66eb2172dbff410f3b661328e119ab73be7
Jim McKeeth <jim@mckeeth.org>
2025-10-05 21:50:13 -0600

    Updated pin mapping

--- keyboards/artsey_io_paintbrush/090/left_hand/keymaps/default/keymap.c
+++ keyboards/artsey_io_paintbrush/090/left_hand/keymaps/default/keymap.c
@@ -28,9 +28,9 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /*
      * ┌───┬───┬───┬───┐
-     * │ A │ B │ C │ D │
+     * │ A │ R │ T │ S │
      * ├───┼───┼───┼───┤
-     * │ E │ F │ G │ H │
+     * │ E │ Y │ I │ O │
      * └───┴───┴───┴───┘
      */
 
